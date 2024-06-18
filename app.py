e72be404994b1fef4a7e4b77d984e91375b7759d
Doaa Essam <119596816+DoaaEssam264@users.noreply.github.com>
2024-06-18 12:51:30 +0000

    Front-end added

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "hello"
-print(__name__)
+def insta_search():
+    return render_template('Home.html')
+  
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
   
