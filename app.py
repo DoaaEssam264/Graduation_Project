cf31ae7929e19cb068c2f02d0ccdc5e4dc380f6b
Afnan Ashraf <120016424+afnanashraf-26@users.noreply.github.com>
2024-06-19 13:00:52 +0000

    loading search enginee

--- app.py
+++ app.py
@@ -1,25 +1,8 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, text
+from flask import Flask, render_template, request
 import os
 from database import load_homepage_random_recommendations
 
 app = Flask(__name__)
-
-# database
-db_connecton_uri = os.environ['db_connection_uri']
-engine = create_engine(db_connecton_uri)
-
-# @app.route("/")
-# def insta_search():
-#     return render_template('Home.html')
-#TO CHECK DB
-# with engine.connect() as conn:
-#     result = conn.execute(text("SELECT category FROM pages"))
-#     usernamess = [row for row in result.all()]
-# print(usernamess)
-
-
 
 #homepage
 #if list of search empty, return random otherwise return pages similars to search
@@ -28,7 +11,19 @@ def home_page():
     pages=load_homepage_random_recommendations()
     # unique_categories=get_unique_categories()
     return render_template('Home.html', recommendations=pages)
-
+    
+@app.route("/search_results",methods=['POST'])
+def serach_results():
+    # data =request.args
+     data =request.form
+    pages=load_search_results(data['query'])
+    # unique_categories=get_unique_categories()
+    return render_template('Product_Search.html', recommendations=pages)
+# @app.route("/product_info")
+# def product_info():
+#     pages=load_homepage_random_recommendations()
+#     # unique_categories=get_unique_categories()
+#     return render_template('Home.html', recommendations=pages)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -4,14 +4,32 @@ import os
 
 db_connecton_uri = os.environ['db_connection_uri']
 engine = create_engine(db_connecton_uri)
+
+#TO CHECK DB
+# with engine.connect() as conn:
+#     result = conn.execute(text("SELECT category FROM pages"))
+#     usernamess = [row for row in result.all()]
+# print(usernamess)
+
+
+#LOAD RECOMENDED PAGES TO HOME PAGE
 def load_homepage_random_recommendations():
     with engine.connect() as conn:
         result = conn.execute(
-            text("SELECT * FROM pages ORDER BY RANDOM() LIMIT 4"))
+            text("SELECT * FROM pages ORDER BY RANDOM() LIMIT 20"))
         pages = []
         for page in result.all():
             pages.append(page._mapping)
-        return pages    
+        return pages 
+
+
+#LOAD PAGES TO USER SEARCH QUERY
+def load_search_results(query):
+    with engine.connect() as conn:  
+        result=conn.execute(text("SELECT * FROM posts"))
+
+
+
 
 # def get_unique_categories():
 #     with engine.connect() as conn:
