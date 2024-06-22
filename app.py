f2e694a78095c25df166620714dfaf18e7ccb668
Doaa Essam <119596816+DoaaEssam264@users.noreply.github.com>
2024-06-22 19:22:10 +0000

    login/logout/signup/ access favorite only when user logged in/logout user

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect ,session
 from database import load_homepage_random_recommendations,load_search_results,add_user,load_user,get_cleaned_categories,get_pages_of_a_certain_category
 from flask_bcrypt import Bcrypt
-
+import os
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
+my_secret = os.environ['SECRET_KEY']
+app.secret_key = my_secret
+
+
+@app.route("/favorite")
+def favorite():
+    if 'loggedin' in session:
+        cleaned_categories = get_cleaned_categories()
+        return render_template('Favorite.html',categories=cleaned_categories)
+    return  redirect(url_for('register'))
+
+
+# @app.route("/chatbot")
+# def chatbot():
+#     if 'loggedin' in session:
+#         cleaned_categories = get_cleaned_categories()
+#         return render_template('Favorite.html',categories=cleaned_categories)
+#     return  redirect(url_for('register'))
 
 @app.route("/register", methods=['GET','POST'])
-def register():
+def register(): 
     if request.method == 'POST':
         form_type = request.form.get('form_type')
         if form_type == 'register':
@@ -29,6 +47,11 @@ def register():
             check=load_user(username)
             if check:
                 if bcrypt.check_password_hash(check[1], password):
+                    # Create session data
+                    session['loggedin'] = True
+                    session['id'] = session.get('id_counter', 1)
+                    session['id_counter'] = session['id'] + 1
+                    session['username'] = username
                     return redirect(url_for('home_page'))
                 else:
                         return redirect(url_for('register'))
@@ -39,6 +62,13 @@ def register():
     return render_template('signup_login.html')
         
 
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('username', None)
+   return redirect(url_for('register'))
 
 @app.route("/")
 def home_page():
@@ -51,13 +81,6 @@ def category(category):
     cleaned_categories = get_cleaned_categories()
     category_pages=get_pages_of_a_certain_category(category.lower())
     return render_template('Category_search.html',categories=cleaned_categories,category_pages=category_pages,category_name=category)
-
-
-
-@app.route("/favorite")
-def favorite():
-    cleaned_categories = get_cleaned_categories()
-    return render_template('Favorite.html',categories=cleaned_categories)
 
 @app.route("/product_info")
 def product_info():
@@ -82,5 +105,3 @@ def search_results():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
-
-# Access all tables from the reflected metadata
