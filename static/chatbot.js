3078fe0d80b9977369638534e80145628411410a
youssraelbatal <134093697+youssraelbatal@users.noreply.github.com>
2024-06-25 21:10:17 +0000

    chatbot frontend + js modification almost done

--- static/chatbot.js
+++ static/chatbot.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "sk-proj-qyW117voX8HE8ZghMGFKT3BlbkFJBzSFQLQKrtax0CjidK6j"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
